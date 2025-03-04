59fe85692cc9c34011f355897f0819d5b812abeb
SpdmTeamX <zusdrodo@gmail.com>
2025-03-04 18:35:26 +0300

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,6 +1,6 @@
 local _env = getgenv and getgenv() or {}
-local Ver = "Version: 4.5v "
-local Tele = "https://t.me/SpdmTeamX"
+local Ver = "Unknown"
+local Tele = "https://t.me/ATX_Hub"
 
 _env.Telegram = Tele
 _env.Version = Ver
